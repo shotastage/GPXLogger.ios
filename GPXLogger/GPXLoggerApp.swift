b783d830366e35f3ab1af6824f2c365da08e7b53
Shota Shimazu <shotastage@users.noreply.github.com>
2021-04-12 14:16:40 +0900

    Initial Commit

---
+++ GPXLogger/GPXLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPXLoggerApp.swift
+//  GPXLogger
+//
+//  Created by Shota Shimazu on 2021/04/12.
+//
+
+import SwiftUI
+
+@main
+struct GPXLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
